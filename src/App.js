476ba1bf608cb442a12fa815b84ef778145f75a7
Hariharan <hari21032001@gmail.com>
2021-05-20 17:21:26 +0530

    added seardchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import Home from "./components/Home";
+import React from 'react';
 
 export default function App() {
   return (
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,46 +1,87 @@
-import React, { useRef } from "react";
+import React, { useRef,useState ,useEffect} from "react";
 import SearchIcon from "@material-ui/icons/Search";
+import '../css/searchbar.module.css';
+import { Typography } from "@material-ui/core";
+
 
 export default function SearchBar() {
-  const searchbar = useRef("");
   const searchinput = useRef("");
+  const searchresult = useRef("");
+  const [searchvalue, setsearchvalue] = useState("");
+  const searchkeyword=["Chennai Super kings","Mumbai Indians","Royal challengers Bangalore","Sunrises Hyderabad","Kolkata Knight Riders","Rajasthan Royals","Punjab Kings","Delhi Capitals"];
+  const [searchkeys,setsearchkeys]=useState(searchkeyword);
 
-  const animatesearchbar = () => {
-    searchbar.current.style.width = "500px";
-    searchinput.current.style.width = "480px";
+  const normalsearchbar = () => { 
+    searchresult.current.style.width = "300px";
+    searchresult.current.style.height = "0px";
   };
-  const normalsearchbar = () => {
-    searchbar.current.style.width = "300px";
-    searchinput.current.style.width = "280px";
-  };
+  
+
+  const handleSearch = (e) => {  
+    setsearchkeys(searchkeyword);
+    const keys=e.target.value;
+    const result=searchkeyword.filter(key=> key.toLowerCase().indexOf(keys)>-1);
+    console.log(result)
+
+    if (e.target.value) {
+      setsearchkeys(result);
+      setsearchvalue(keys);
+      searchresult.current.style.width = "520px";
+      searchresult.current.style.height = "300px";
+    }
+    else {
+      setsearchkeys(result);
+      setsearchvalue(e.target.value);
+      searchresult.current.style.height = "0px";
+    }
+  }
+  const handleclickedsearch=(value)=>{
+   setsearchvalue(value);
+   normalsearchbar();
+   setsearchkeys(searchkeyword);
+   console.log(searchkeys);
+  }
+
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        background: "#605e7c",
-        borderRadius: "5px",
-        padding: "10px",
-        width: "300px",
-        transition: ".5s"
-      }}
-      ref={searchbar}
-    >
-      <SearchIcon />
-      <input
-        ref={searchinput}
+    <div>
+      <div
         style={{
-          background: "transparent",
-          color: "white",
-          fontSize: "16px",
-          border: "none",
-          outline: "none"
+          display: "flex",
+          alignItems: "center",
+          background: "#605e7c",
+          borderRadius: "5px",
+          padding: "10px",
+          width: "500px",
+          transition: ".5s"
         }}
-        type="search"
-        placeholder="Search..."
-        onFocus={animatesearchbar}
-        onBlur={normalsearchbar}
-      />
+      >
+        <SearchIcon />
+        <input
+          ref={searchinput}
+          style={{
+            background: "transparent",
+            color: "white",
+            fontSize: "16px",
+            border: "none",
+            outline: "none",
+          width: "480px"
+          }}
+          type="text"
+          onChange={handleSearch}
+          placeholder="Search..."
+          value={searchvalue}
+        />
+      </div>
+      <div ref={searchresult} style={{ transition:".2s",overflow:"hidden",overflowY:"auto",position: "absolute", marginTop: "10px", background: "white", color: "black", width: "520px",height:0, borderRadius: "5px", boxShadow: "0px 5px 5px" }}>
+        <ul >
+          {
+            searchkeys.map(s=>{
+              return  <li onClick={()=>handleclickedsearch(`${s}`)}><SearchIcon></SearchIcon><Typography variant="body1">{s}</Typography></li>
+         
+            })
+          }
+       </ul>
+      </div>
     </div>
   );
 }
